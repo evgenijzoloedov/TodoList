aec00d664320f9c6f7748d0d1b3a28352e3b0730
Evgenijzoloedov <evgenijzoloedov@gmail.com>
2021-02-13 22:11:26 +0300

    someproblems

--- index.js
+++ index.js
@@ -22,133 +22,134 @@ const number__done=document.querySelector('.number_done')
         tasks__todo.appendChild(task)
         number__todo.textContent=tasks__todo.childNodes.length-1
         inputtext.value=''
-    })
-
-
-
-tasks__todo.addEventListener('click',e=>{
-    tasks__todo.childNodes.forEach(task=>{
-        task.childNodes.forEach(cheakboxtitle=>{
-            if (cheakboxtitle.className==='remove'){
-                
-                cheakboxtitle.addEventListener('click',remove=>{
-                    console.log('remove');
-                    task.remove()
-                    number__todo.textContent=tasks__todo.childNodes.length-1
-                })
-            }
-            if (cheakboxtitle.className==='cheakboxtitle'){
-                cheakboxtitle.childNodes.forEach(cheakbox=>{
-                    if (cheakbox.className==='cheakbox'){
-                        cheakbox.addEventListener('click', ()=>{
-                            if (cheakbox.checked){
-                                number__todo.textContent=tasks__todo.childNodes.length-1
-                                task.remove()
-                                tasks__done.appendChild(task)
-                                number__done.textContent=tasks__done.childNodes.length -3
+        tasks__todo.addEventListener('click',e=>{
+            tasks__todo.childNodes.forEach(task=>{
+                task.childNodes.forEach(cheakboxtitle=>{
+                    if (cheakboxtitle.className==='remove'){
+                        
+                        cheakboxtitle.addEventListener('click',remove=>{
+                            console.log('remove');
+                            task.remove()
+                            number__todo.textContent=tasks__todo.childNodes.length-1
+                        })
+                    }
+                    if (cheakboxtitle.className==='cheakboxtitle'){
+                        cheakboxtitle.childNodes.forEach(cheakbox=>{
+                            if (cheakbox.className==='cheakbox'){
+                                cheakbox.addEventListener('click', ()=>{
+                                    if (cheakbox.checked){
+                                        number__todo.textContent=tasks__todo.childNodes.length-1
+                                        task.remove()
+                                        tasks__done.appendChild(task)
+                                        number__done.textContent=tasks__done.childNodes.length -3
+                                    }
+                                })
                             }
                         })
                     }
                 })
-            }
+            })
         })
-    })
-})
-tasks__done.addEventListener('click',e=>{
-    tasks__done.childNodes.forEach(task=>{
-        task.childNodes.forEach(cheakboxtitle=>{
-            if (cheakboxtitle.className==='remove'){
-                
-                cheakboxtitle.addEventListener('click',remove=>{
-                    console.log('remove');
-                    task.remove()
-                    number__done.textContent=tasks__done.childNodes.length-3
-                })
-            }
-            if (cheakboxtitle.className==='cheakboxtitle'){
-                cheakboxtitle.childNodes.forEach(cheakbox=>{
-                    if (cheakbox.className==='cheakbox'){
-                        cheakbox.addEventListener('click', ()=>{
-                            if (!cheakbox.checked){
-                                number__done.textContent=tasks__done.childNodes.length -3
-                                task.remove()
-                                tasks__todo.appendChild(task)
-                                number__todo.textContent=tasks__todo.childNodes.length -1
+        tasks__done.addEventListener('click',e=>{
+            tasks__done.childNodes.forEach(task=>{
+                task.childNodes.forEach(cheakboxtitle=>{
+                    if (cheakboxtitle.className==='remove'){
+                        
+                        cheakboxtitle.addEventListener('click',remove=>{
+                            console.log('remove');
+                            task.remove()
+                            number__done.textContent=tasks__done.childNodes.length-3
+                        })
+                    }
+                    if (cheakboxtitle.className==='cheakboxtitle'){
+                        cheakboxtitle.childNodes.forEach(cheakbox=>{
+                            if (cheakbox.className==='cheakbox'){
+                                cheakbox.addEventListener('click', ()=>{
+                                    if (!cheakbox.checked){
+                                        number__done.textContent=tasks__done.childNodes.length -3
+                                        task.remove()
+                                        tasks__todo.appendChild(task)
+                                        number__todo.textContent=tasks__todo.childNodes.length -1
+                                    }
+                                })
                             }
                         })
                     }
                 })
-            }
-        })
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-tasks__todo.childNodes.forEach(el=>{
-    el.childNodes.forEach(e=>{
-        if (e.className==='remove'){
-            e.addEventListener('click',()=>{
-                el.remove()
-                number__todo.textContent=tasks__todo.childNodes.length - 1                
-            })
-        }
-        e.childNodes.forEach(elem=>{ 
-            elem.addEventListener('click',()=>{
-                if (elem.checked){
-                    el.remove()
-                    tasks__done.append(el)
-                    number__todo.textContent=tasks__todo.childNodes.length - 1
-                    number__done.textContent=tasks__done.childNodes.length -3
-                }
             })
         })
     })
-})
-tasks__done.childNodes.forEach(el=>{
-    el.childNodes.forEach(e=>{
-        console.log('in tasksDone', e);
 
-        if (e.className==='remove'){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// tasks__todo.childNodes.forEach(el=>{
+//     el.childNodes.forEach(e=>{
+//         if (e.className==='remove'){
+//             e.addEventListener('click',()=>{
+//                 el.remove()
+//                 number__todo.textContent=tasks__todo.childNodes.length - 1                
+//             })
+//         }
+//         e.childNodes.forEach(elem=>{ 
+//             elem.addEventListener('click',()=>{
+//                 if (elem.checked){
+//                     el.remove()
+//                     tasks__done.append(el)
+//                     number__todo.textContent=tasks__todo.childNodes.length - 1
+//                     number__done.textContent=tasks__done.childNodes.length -3
+//                 }
+//             })
+//         })
+//     })
+// })
+// tasks__done.childNodes.forEach(el=>{
+//     el.childNodes.forEach(e=>{
+//         console.log('in tasksDone', e);
+
+//         if (e.className==='remove'){
             
-            e.addEventListener('click',()=>{
-                el.remove()
+//             e.addEventListener('click',()=>{
+//                 el.remove()
                            
-            })
-        }
+//             })
+//         }
         
-    })
-})
+//     })
+// })
